379ed8a773e936d8828ddd3dbf9bbb1d283ebf32
500060722 <500060722@stu.upes.ac.in>
2019-10-04 23:39:26 +0530

    Working with the Strings

---
+++ Conditional_Statements/src/Conditional_statements.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main() {
+    val sc=Scanner(System.`in`)
+    val s1: String = "My name is Prajjawal"
+    val s2: String= sc.nextLine()
+    println(s1)
+    println(s2)
+}
