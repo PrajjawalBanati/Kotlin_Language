698abc2e91bb4bc4b2d6cd195edcec313a6e7b4b
500060722 <500060722@stu.upes.ac.in>
2019-10-04 23:51:20 +0530

    Woking with IF statement in KOTLIN

--- Conditional_Statements/src/Conditional_statements.kt
+++ Conditional_Statements/src/Conditional_statements.kt
@@ -1,9 +1,32 @@
 import java.util.*
 
+fun max(a:Int,b:Int):Int{
+    if(a>b)
+        return a
+    else if(b>a)
+        return b
+    else
+        return 0
+}
 fun main() {
+    val a: Int
+    val b: Int
+    val c: Int
     val sc=Scanner(System.`in`)
     val s1: String = "My name is Prajjawal"
+    println("Enter any String")
     val s2: String= sc.nextLine()
     println(s1)
     println(s2)
+    println("Enter two numbers")
+    a=sc.nextInt()
+    b=sc.nextInt()
+    c=max(a,b)
+    if(c==0)
+    {
+        println("Numbers are equal")
+    }
+    else
+        println("Higher number is $c")
+
 }
