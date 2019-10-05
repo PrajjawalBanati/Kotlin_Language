d736ac945dfde9d478e1637fdc24769eadfa5985
500060722 <500060722@stu.upes.ac.in>
2019-10-05 15:07:37 +0530

    Illustration to understand use of for and while loop over a list in Kotlin

--- Loops/src/Loops.kt
+++ Loops/src/Loops.kt
@@ -1,8 +1,23 @@
+fun Iteration(){
+    var a= listOf<String>("Prajjawal","Prakhar","Nirmaljeet","Sajal")
+    var b= listOf<String>("apple","mango","banana","peach","Cucumber")
+    var c: Int=0
+    for(item in a)
+    {
+        println(item)
+    }
+    while(c<a.size)
+    {
+        println(b[c])
+        c++
+    }
+
+}
 fun main() {
-    val x:Int=10
-    val y:Int=9
+    var x:Int=10
     for (y in 1..x)
     {
         println("$y fits in the range of x")
     }
+    Iteration()
 }
