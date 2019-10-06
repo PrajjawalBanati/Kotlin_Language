233aafce616bad7cb1ab0c48afdcb4d3e2166568
500060722 <500060722@stu.upes.ac.in>
2019-10-06 17:04:42 +0530

    Bitwise Operators in Kotlin

---
+++ Basics/src/Operators.kt
@@ -0,0 +1,23 @@
+fun main(){
+    // shl means shift left
+    val a=(2 shl 2)
+    println(a)
+    val b=(-2 shl 2)
+    println(b)
+    // shr means shift right
+    val c=(2 shr 2)
+    println(c)
+    val d=(-2 shr 2)
+    println(d)
+    // and means bitwise and
+    val e=(2 shl 2) and 3
+    println(e)
+    // or means bitwise or
+    val f=(2 shl 2) or 3
+    println(f)
+    // xor conversion
+    val g=(2 shl 2) xor 3
+    println(g)
+
+
+}
