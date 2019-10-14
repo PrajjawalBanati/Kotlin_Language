fa205bed79611ebc6098f3abbbf6a46be03438dd
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2019-10-14 16:39:56 +0330

    Update Conditional_statements.kt
    adding a return statement for 'when' expression

--- Conditional_Statements/src/Conditional_statements.kt
+++ Conditional_Statements/src/Conditional_statements.kt
@@ -1,44 +1,52 @@
 import java.util.*
-// USING IF STATEMENT
-fun max(a:Int,b:Int):Int{
-    if(a>b)
-        return a
-    else if(b>a)
-        return b
-    else
-        return 0
-}
-// USING WHEN STATEMENT
-fun show(a: Int){
-    when(a%2)
-    {
-        0->
-        println("String Length is Even")
-        1->
-        println("String Length is Odd")
+    // USING IF STATEMENT
+    fun max(a: Int, b: Int): Int {
+        if (a > b)
+            return a
+        else if (b > a)
+            return b
+        else
+            return 0
     }
-}
-fun main() {
-    val a: Int
-    val b: Int
-    val c: Int
-    val sc=Scanner(System.`in`)
-    val s1: String = "My name is Prajjawal"
-    println("Enter any String")
-    val s2: String= sc.nextLine()
-    println(s1)
-    println(s2)
-    if(s2 is String && s2.length>0)
-        show(s2.length)
-    println("Enter two numbers")
-    a=sc.nextInt()
-    b=sc.nextInt()
-    c=max(a,b)
-    if(c==0)
-    {
-        println("Numbers are equal")
-    }
-    else
-        println("Higher number is $c")
 
-}
+    // USING WHEN STATEMENT
+    fun show(a: Int) {
+        when (a % 2) {
+            0 ->
+                println("String Length is Even")
+            1 ->
+                println("String Length is Odd")
+        }
+    }
+
+    //Both 'When' and 'If' are expressions 
+    fun isOdd(a:Int): Boolean {
+        return when (a % 2) {
+            0 -> false
+            1 -> true
+            else -> throw RuntimeException("NOT A real number")
+        }
+    }
+    
+    fun main() {
+        val a: Int
+        val b: Int
+        val c: Int
+        val sc = Scanner(System.`in`)
+        val s1: String = "My name is Prajjawal"
+        println("Enter any String")
+        val s2: String = sc.nextLine()
+        println(s1)
+        println(s2)
+        if (s2 is String && s2.length > 0)
+            show(s2.length)
+        println("Enter two numbers")
+        a = sc.nextInt()
+        b = sc.nextInt()
+        c = max(a, b)
+        if (c == 0) {
+            println("Numbers are equal")
+        } else
+            println("Higher number is $c")
+
+    }
