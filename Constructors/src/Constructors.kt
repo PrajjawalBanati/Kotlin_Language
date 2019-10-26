c63e27f6f7fab2466c58bce76e4f0c7bc2eb7c9c
500060722 <500060722@stu.upes.ac.in>
2019-10-26 22:32:41 +0530

    Modified Constructors.kt(added one more primary constructor)

--- Constructors/src/Constructors.kt
+++ Constructors/src/Constructors.kt
@@ -2,7 +2,6 @@
 class Person(firstName: String){
 
     var lastName = "notSetYet"
-
     // init function is running for primary constructor
     init {
         println("Primary constructor: first name is $firstName and last name is $lastName")
@@ -14,7 +13,20 @@ class Person(firstName: String){
         println("Secondary constructor: first name is $firstName and last name is $lastName")
     }
 }
+class box constructor(length: Int,breadth: Int){
+    private var length:Int
+    private var breadth:Int
+    //The primary constructor cannot contain any code. Initialization code can be placed in initializer
+    //blocks, which are prefixed with the init keyword.
+    init{
+        this.length=length
+        this.breadth=breadth
+        println("Area of the box is "+(this.length*this.breadth))
+    }
+
+}
 
 fun main(args: Array<String>){
     var person = Person("Jalal","Khademi")
+    var obj=box(23,34)
 }
